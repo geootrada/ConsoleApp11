29b1f7eee8036e6efdd12d18ac1571a863e9d92a
geootrada <geootrada@yandex.ru>
2024-03-16 17:11:43 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message;
+            int numberOfTimes;
+
+            Console.Write(" Здравствуйте, это программа-напоминатель!\n " +
+               "Введите сообщение, которое вам нужно сегодня напомнить: ");
+            //Позвонить в поликлинику
+            message = Console.ReadLine();
+
+            Console.Write("Сколько раз вам напомнить: ");
+            numberOfTimes = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < numberOfTimes; i++)
+            {
+                Console.WriteLine(message);
+            }
+        }   
+    }
+}
